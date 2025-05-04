6d071c9f271d7d9dc0f0ffd21db9edd0b9de6ba0
adarsh1701198 <adarsh.godabole@tarento.com>
2025-05-04 18:41:45 +0530

    Multiple route handlers

--- src/app.js
+++ src/app.js
@@ -2,9 +2,46 @@ const express = require("express");
 
 const app = express();
 
-app.use("/test", (req, res) => {
-  res.send("HELL)");
+// ROUTE HANDLERS ////
+
+app.get("/user/:uid", (req, res) => {
+  console.log("REQUEST", req.query);
+  console.log("REQUEST", req.params);
+
+  res.send({ firstName: "Adarsh" });
 });
+
+app.post("/user", (req, res) => {
+  res.send("Saved");
+});
+
+app.post("/deleted", (req, res) => {
+  res.send("Deleted");
+});
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////
+
+/// Dont send any response --------------------------> INFINITE
+app.get("/user/:uid", (req, res) => {
+  console.log("REQUEST", req.query);
+  console.log("REQUEST", req.params);
+});
+/////////////////////////////////////////////////////////////////
+
+// Multiple rount handlers ------------------------> Gives 1st
+// No res.send in 1st then Infinite
+// Cant write next after first send -------> Cant send 2 res to same client ---- Error [ERR_HTTP_HEADERS_SENT]: Cannot set headers after they are sent to the client
+
+app.get(
+  "/user",
+  (req, res, next) => {
+    res.send({ firstName: "Adarsh" });
+    next();
+  },
+  (req, res) => {
+    res.send({ lastName: "Godbole" });
+  }
+);
 
 app.listen(3000, () => {
   console.log("RUNNING");
